6462f5324204e627427fb0bbf7dc999621a71213
ilhamshf <shevarajacoc@gmail.com>
2021-08-22 19:15:59 +0700

    update ui

---
+++ src/components/FooterSection.tsx
@@ -0,0 +1,30 @@
+function Footer() {
+  return (
+    <footer className="mt-4 text-sm text-center">
+      <p>
+        Dibuat oleh{' '}
+        <a
+          href="https://ilhammshf.my.id"
+          target="_blank"
+          rel="noopenner noreferrer"
+          className="font-bold text-link"
+        >
+          Ilham Shofa
+        </a>
+      </p>
+      <p className="mt-2">
+        Cek juga:{' '}
+        <a
+          href="https://hamsh.me"
+          target="_blank"
+          rel="noopenner noreferrer"
+          className="font-bold text-link"
+        >
+          Hamsh.me
+        </a>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
